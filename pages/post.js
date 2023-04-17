9ff3f189b340e9c9a45d7a040f950b089e51c052
vishu bansal <bansalvishu56@gmail.com>
2023-04-17 18:16:56 +0530

    added edit functinoality

--- pages/post.js
+++ pages/post.js
@@ -2,66 +2,102 @@ import { auth, db } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import {toast} from "react-toastify";
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
-export default function Post(){
+export default function Post() {
+  const route = useRouter();
 
-    const route = useRouter();
+  //State of form
+  const [post, setPost] = useState({ description: "" });
 
-    //State of form
-    const [post, setPost] = useState({description:""});
+  //user defining
+  const [user, loading] = useAuthState(auth);
 
-    //user defining
-    const[user, loading] = useAuthState(auth);
+  //Update Data when dashboard edit is pressed
+  const routeData = route.query;
 
-    //submit post
-    const submitPost = async(e) =>{
-        e.preventDefault();
+  //submit post
+  const submitPost = async (e) => {
+    e.preventDefault();
 
-        //Run Checks for description
-        if(!post.description){
-            toast.error("Description Field Empty 😅", {
-                position: toast.POSITION.TOP_CENTER,
-                autoClose: 1500,
-            });
-            return;
-        }
-        if(post.description.length > 300 ){
-            toast.error("Description too long 😅", {
-                position: toast.POSITION.TOP_CENTER,
-                autoClose: 1500,
-            });
-            return;
-        }
-
-        //Submit the post
-        const collectionRef = collection(db, 'posts');
-        await addDoc(collectionRef, {
-            ...post,
-            timestamp: serverTimestamp(),
-            user: user.uid,
-            avatar: user.photoURL,
-            username: user.displayName,
-        })
-        setPost({description:""})
-        return route.push("/")
+    //Run Checks for description
+    if (!post.description) {
+      toast.error("Description Field Empty 😅", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      return;
+    }
+    if (post.description.length > 300) {
+      toast.error("Description too long 😅", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      return;
     }
 
-    return(
-        <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
-            <form onSubmit={submitPost}>
-                <h1 className="text-2xl font-bold">Create a new post</h1>
-                <div className="py-2">
-                    <h3 className="text-lg font-medium py-2">Description</h3>
-                    <textarea
-                    value={post.description}
-                    onChange={(e)=>setPost({...post, description: e.target.value})}
-                    className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm"></textarea>
-                    <p className={`text-cyan-600 font-medium text-sm ${post.description.length > 300 ? 'text-red-600': ''}`}>{post.description.length}/300</p>
-                </div>
-                <button type="submit" className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm">Submit</button>
-            </form>
+    //Update existing post
+    if (post?.hasOwnProperty("id")) {
+      const docRef = doc(db, "posts", post.id);
+      const updatedPost = { ...post, timestamp: serverTimestamp() };
+      await updateDoc(docRef, updatedPost);
+      return route.push("/");
+    } else {
+      //Submit the post
+      const collectionRef = collection(db, "posts");
+      await addDoc(collectionRef, {
+        ...post,
+        timestamp: serverTimestamp(),
+        user: user.uid,
+        avatar: user.photoURL,
+        username: user.displayName,
+      });
+      setPost({ description: "" });
+      return route.push("/");
+    }
+  };
+  //Check our user for edit option pressed by user
+  const checkUser = async () => {
+    if (loading) return;
+    if (!user) route.push("/auth/login");
+    if (routeData.id) {
+      setPost({ description: routeData.description, id: routeData.id });
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, [user, loading]);
+
+  return (
+    <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
+      <form onSubmit={submitPost}>
+        <h1 className="text-2xl font-bold">
+          {post.hasOwnProperty("id") ? "Edit your post" : "Create a new post"}
+        </h1>
+        <div className="py-2">
+          <h3 className="text-lg font-medium py-2">Description</h3>
+          <textarea
+            value={post.description}
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
+            className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm"
+          ></textarea>
+          <p
+            className={`text-cyan-600 font-medium text-sm ${
+              post.description.length > 300 ? "text-red-600" : ""
+            }`}
+          >
+            {post.description.length}/300
+          </p>
         </div>
-    )
+        <button
+          type="submit"
+          className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }

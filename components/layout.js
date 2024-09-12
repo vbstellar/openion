0a559b90f97891caf6d844be73aea3a9e28f97a0
Vishu Bansal <bansalvishu56@gmail.com>
2024-09-12 10:28:30 +0530

    Padding fix

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,9 @@ export default function Layout({ children }) {
   return (
     <div className="mx-0 md:max-w-full md:mx-auto font-poppins h-screen">
       <Nav />
-      <main className="mx-6 mb-auto md:max-w-2xl md:mx-auto">{children}</main>
+      <main className="mx-6 mb-auto md:max-w-2xl md:mx-auto pb-12">
+        {children}
+      </main>
       <Footer />
     </div>
   );
